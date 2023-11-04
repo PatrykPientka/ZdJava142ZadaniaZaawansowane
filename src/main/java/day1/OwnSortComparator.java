95c21835a6f24873ae6d9bc0325fa8679625c685
Patryk Pientka <pientka.patryk@gmail.com>
2023-11-04 09:54:37 +0000

    Task1 Solution

---
+++ src/main/java/day1/OwnSortComparator.java
@@ -0,0 +1,11 @@
+package day1;
+
+import java.util.Comparator;
+
+public class OwnSortComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.compareTo(o1);
+    }
+}
--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -1,14 +1,67 @@
 package day1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        List<String> unsorted = List.of("a","b" , "c", "d");
+        List<String> sorted = sortUsingAnonymousClass(unsorted);
+        List<String> sorted2 = sortUsingOwnComparator(unsorted);
+        List<String> sorted3 = sortUsingLambda(unsorted);
+        List<String> sorted4 = sortUsingReverseOrder(unsorted);
+        List<String> sorted5 = sortOnList(unsorted);
+        List<String> sorted6 = sortUsingStream(unsorted);
+        System.out.println(sorted);
+        System.out.println(sorted2);
+        System.out.println(sorted3);
+        System.out.println(sorted4);
+        System.out.println(sorted5);
+        System.out.println(sorted6);
     }
 
     public static List<String> sortUsingAnonymousClass(List<String> unsorted){
+        List<String > sorted = new ArrayList<>(unsorted);
+        Collections.sort(sorted, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
 
-        return null;
+                return o2.compareTo(o1);
+            }
+        });
+        return sorted;
+    }
+
+    public static List<String> sortUsingOwnComparator(List<String> unsorted){
+        List<String > sorted = new ArrayList<>(unsorted);
+        Collections.sort(sorted, new OwnSortComparator());
+        return sorted;
+    }
+
+    public static List<String> sortUsingLambda(List<String> unsorted){
+        List<String > sorted = new ArrayList<>(unsorted);
+        Collections.sort(sorted,(o1, o2) -> o2.compareTo(o1));
+        return sorted;
+    }
+    public static List<String> sortUsingReverseOrder(List<String> unsorted){
+        List<String > sorted = new ArrayList<>(unsorted);
+        Collections.sort(sorted, Comparator.reverseOrder());
+        return sorted;
+    }
+
+    public static List<String> sortOnList(List<String> unsorted){
+        List<String > sorted = new ArrayList<>(unsorted);
+        sorted.sort(Comparator.reverseOrder());
+        return sorted;
+    }
+    public static List<String> sortUsingStream(List<String> unsorted){
+        List<String > sortedCollections = new ArrayList<>(unsorted);
+
+        return sortedCollections.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 }
