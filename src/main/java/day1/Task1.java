3f78cc8c7ad89a64fe653454f962189653dceb2c
Patryk Pientka <pientka.patryk@gmail.com>
2023-11-04 09:07:23 +0000

    Test commit

---
+++ src/main/java/day1/Task1.java
@@ -0,0 +1,14 @@
+package day1;
+
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> sortUsingAnonymousClass(List<String> unsorted){
+
+        return null;
+    }
+}
