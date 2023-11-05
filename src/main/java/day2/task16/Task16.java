59da484790b41dc55a77cb81ee9936e24e891b9b
Patryk Pientka <pientka.patryk@gmail.com>
2023-11-05 11:38:47 +0000

    Task16 Solution

---
+++ src/main/java/day2/task16/Task16.java
@@ -0,0 +1,15 @@
+package day2.task16;
+
+public class Task16 {
+    public static void main(String[] args) {
+        Runner fitnessLevel1 = Runner.getFitnessLevel(300);
+        System.out.println(fitnessLevel1);
+
+        Runner fitnessLevel2 = Runner.getFitnessLevel(256);
+        System.out.println(fitnessLevel2);
+
+        Runner fitnessLevel3 = Runner.getFitnessLevel(-512);
+        System.out.println(fitnessLevel3);
+
+    }
+}
